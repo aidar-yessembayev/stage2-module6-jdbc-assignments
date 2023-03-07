043da6bb67a64dd7055ff7d39510945237bc73f5
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-03-07 17:45:39 +0600

    Update CustomConnector.java

--- src/main/java/jdbc/CustomConnector.java
+++ src/main/java/jdbc/CustomConnector.java
@@ -1,11 +1,30 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class CustomConnector {
+    static Connection connection;
+
     public Connection getConnection(String url) {
+        try {
+            connection = DriverManager.getConnection(url);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return connection;
     }
 
-    public Connection getConnection(String url, String user, String password)  {
+    public Connection getConnection(String url, String user, String password) {
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
